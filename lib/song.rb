28c3fe6485968d4d0e61578f2fd1ce0cc809cfc1
Ray Ockenfels <ockenfelsr@gmail.com>
2020-07-05 20:33:38 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,28 @@
+class Song
+  
+  attr_accessor :name, :artist
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.new_by_filename(name)
+    data = name.slice(0, name.length - 4).split(" - ")
+    song = Song.new(data[1])
+    artist = Artist.find_or_create_by_name(data[0])
+    Artist.add_song(song)
+    binding.pry
+  end
+  
+  def artist_name(name)
+    your_artist = Artist.find_or_create_by_name(name)
+    self.artist = your_artist
+  end
+end
